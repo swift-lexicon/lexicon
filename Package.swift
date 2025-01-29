2c3f04c3e006abc01727e42ee197ad865efad675
Aaron Vranken <23616042+AaronVr@users.noreply.github.com>
2025-01-29 20:30:51 +0700

    Changed package name to lowercase

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 import CompilerPluginSupport
 
 let package = Package(
-    name: "Lexicon",
+    name: "lexicon",
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
     products: [
         .library(
