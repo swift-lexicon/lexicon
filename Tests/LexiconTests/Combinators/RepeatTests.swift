4002246824afc399a5375b45a01016589aaa8e1c
AaronVr <23616042+AaronVr@users.noreply.github.com>
2025-02-07 20:25:00 +0700

    Added some more tests for Repeat parser

--- Tests/LexiconTests/Combinators/RepeatTests.swift
+++ Tests/LexiconTests/Combinators/RepeatTests.swift
@@ -22,6 +22,61 @@ final class RepeatTests: XCTestCase {
         XCTAssertEqual(result?.remaining.count, 0)
     }
     
+    func testRepeatValidChildSucceedsForMinBound() throws {
+        let repetitions = 2
+        let input = Array(repeating: "a", count: repetitions).joined()
+        
+        let parser = Repeat(min: 2) {
+            Character("a")
+        }
+        
+        let result = try parser.parse(input)
+                
+        XCTAssertEqual(result?.output.count, repetitions)
+        XCTAssertEqual(result?.remaining.count, 0)
+    }
+    
+    func testRepeatValidChildSucceedsBetweenBound() throws {
+        let repetitions = 3
+        let input = Array(repeating: "a", count: repetitions).joined()
+        
+        let parser = Repeat(min: 2, max: 4) {
+            Character("a")
+        }
+        
+        let result = try parser.parse(input)
+                
+        XCTAssertEqual(result?.output.count, repetitions)
+        XCTAssertEqual(result?.remaining.count, 0)
+    }
+    
+    func testRepeatValidChildFailsLessThanMinBound() throws {
+        let repetitions = 2
+        let input = Array(repeating: "a", count: repetitions).joined()
+        
+        let parser = Repeat(min: 3, max: 4) {
+            Character("a")
+        }
+        
+        let result = try parser.parse(input)
+                
+        XCTAssertNil(result)
+    }
+    
+    func testRepeatValidChildHasRemainderForMoreThanMaxBound() throws {
+        let repetitions = 5
+        let input = Array(repeating: "a", count: repetitions).joined()
+        
+        let parser = Repeat(min: 3, max: 4) {
+            Character("a")
+        }
+        
+        let result = try parser.parse(input)
+
+        XCTAssertEqual(result?.output.count, 4)
+        XCTAssertEqual(result?.remaining.count, 1)
+    }
+    
     func testRepeatWithSeparatorReturnsCorrectAmount() throws {
         let repetitions = 10
         let input = Array(repeating: "a", count: repetitions).joined(separator: ",")
